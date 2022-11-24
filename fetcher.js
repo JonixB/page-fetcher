dd3ef75b0217f900045d90ce4fdc492a8027efbe
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-23 18:29:51 -0800

    a mini code that accesses a website given from an argument and downloads the body of the response into the local path

---
+++ fetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const fs = require('fs');
+
+const URL = process.argv[2];
+const filePath = process.argv[3];
+
+request(`${URL}`, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // // console.log('body:', body);
+
+  if (response.statusCode !== 200) {
+    console.log(`The URL given was invalid. Please give a valid URL.`)
+    return;
+  }
+
+  fs.writeFile(`${filePath}`, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    const fileSize = fs.statSync(`${filePath}`);
+    const size = fileSize.size;
+    console.log(`Downloaded and saved ${size} bytes to ${filePath}`);
+  });
+});
